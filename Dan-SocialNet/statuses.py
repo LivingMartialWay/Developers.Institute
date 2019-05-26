ca210735a44c3a8b4c3841fa801c7687441440ca
Dan <50657694+LivingMartialWay@users.noreply.github.com>
2019-05-26 10:45:31 +0300

    Add files via upload
    This is *MY* version of the SocialNet project, not the completed version. I did about 60-70%, and all alone.

---
+++ Dan-SocialNet/statuses.py
@@ -0,0 +1,49 @@
+def get_status(username):
+    statuses = {}
+    for status in statuses:
+        if status['user_id'] == username:
+            stautes.append['text']['user_id']
+    return statuses
+
+statuses = [
+  {
+    'user_id': 1,
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    'user_id': 1,
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    'user_id': 1,
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    'user_id': 1,
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    'user_id': 2,
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    'user_id': 2,
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    'user_id': 5,
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    'user_id': 9,
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    'user_id': 9,
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    'user_id': 9,
+    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+]
